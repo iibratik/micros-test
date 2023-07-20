201c46bf63e14b6cb624167506e66552feaefc81
iibratik <iibragimov2417@gamil.com>
2023-07-20 22:14:44 +0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({ 
+    vue({
       template: { transformAssetUrls }
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
@@ -35,4 +35,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  publicPath: process.env.NODE_ENV === 'production' ? '/micros-test/' : '/',
 })
