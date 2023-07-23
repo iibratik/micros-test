e52c1b8130044778af5c266e9e08fcab205f52db
iibratik <iibragimov2417@gamil.com>
2023-07-24 00:21:11 +0500

    title icon change

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,61 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WorkerDetails from '@/views/Details/WorkerDetails.vue';
-import DocumentDetails from '@/views/Details/DocumentDetails.vue';
-import WorkersList from "@/views/Lists/WorkersList"
-import DocumentsList from "@/views/Lists/DocumentsList"
-import TestPage from "@/views/TestPage.vue"
-
+import WorkerDetails from '@/views/Details/WorkerDetails.vue'
+import DocumentDetails from '@/views/Details/DocumentDetails.vue'
+import WorkersList from '@/views/Lists/WorkersList'
+import DocumentsList from '@/views/Lists/DocumentsList'
 
 const routes = [
   {
     path: '/',
-    redirect: '/workers', // Пустой список работников, здесь они будут загружаться из хранилища
-
+    redirect: '/workers',
+    meta: { title: 'Список работников' },
+    // Пустой список работников, здесь они будут загружаться из хранилища
   },
   // Маршрут для отображения списка работников
   {
     path: '/workers',
     component: WorkersList,
+    meta: { title: 'Список работников' },
     props: { workers: [] }, // Пустой список работников, здесь они будут загружаться из хранилища
   },
   // Маршрут для отображения списка документов
   {
     path: '/documents',
     component: DocumentsList,
+    meta: { title: 'Список документов' },
     props: { documents: [] }, // Пустой список документов, здесь они будут загружаться из хранилища
   },
   // Параметризированный маршрут для отображения данных о работнике
   {
     path: '/worker/:id',
     component: WorkerDetails,
+    meta: { title: 'Рабочий' },
     props: true,
   },
   // Параметризированный маршрут для отображения данных о документе
   {
     path: '/document/:id',
     component: DocumentDetails,
+    meta: { title: 'Документ' },
     props: true,
-  },
-  {
-    path: '/test',
-    component: TestPage,
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
+
+// Глобальный навигационный хук `afterEach`
+router.afterEach((to) => {
+  if (to.meta.title) {
+    document.title = to.meta.title;
+    localStorage.setItem('pageTitle', to.meta.title); // Сохранение заголовока в localStorage
+  } else {
+    document.title = 'micros test'; // title по default
+    localStorage.removeItem('pageTitle'); // Удаление сохраненного заголовока из localStorage
+  }
+});
+
 
 export default router
